19cd2c08ee43a7e3b7e122e37d095d207d1b3f23
mervetzouari <120953183+mervetzouari@users.noreply.github.com>
2022-12-19 13:42:40 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import "./bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.js";
 import "./assets/main.css";
 import "./css/main.css";
 
