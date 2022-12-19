6e6bd9b9897186c6dd096898b38dbd355ba3bdbf
mervetzouari <mervetz2502@gmail.com>
2022-12-19 10:52:19 +0100

    modif config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+base : '/dodo1/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
